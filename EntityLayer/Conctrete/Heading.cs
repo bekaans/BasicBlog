fca6f5e184c07424ce9e9ba59d38ff48146dbcf3
Berke Kaan <bekaans@icloud.com>
2023-08-19 00:54:19 +0300

    Add ICollection -- Heading <=> Content

--- EntityLayer/Conctrete/Heading.cs
+++ EntityLayer/Conctrete/Heading.cs
@@ -11,5 +11,11 @@ namespace EntityLayer.Conctrete
         public int HeadingID { get; set; }
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public  ICollection<Content> Contents { get; set; }
+
     }
 }
