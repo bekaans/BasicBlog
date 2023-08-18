b50ce5db63d16e53927c62e0ff7dde5ec7d69a55
Berke Kaan <bekaans@icloud.com>
2023-08-19 00:36:39 +0300

    Create Content Class and props

---
+++ EntityLayer/Conctrete/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Conctrete
+{
+    public class Content
+    {
+        public int ContentID { get; set; }
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+    }
+}
