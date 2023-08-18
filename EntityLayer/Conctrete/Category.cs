c7db2883019bffb9f73a42655b4ce66f77d62e53
Berke Kaan <bekaans@icloud.com>
2023-08-19 00:54:57 +0300

    Add Category ICollection -- Category <=> Heading

--- EntityLayer/Conctrete/Category.cs
+++ EntityLayer/Conctrete/Category.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Conctrete
 {
-    internal class Category
+    public class Category
     {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
     }
 }
