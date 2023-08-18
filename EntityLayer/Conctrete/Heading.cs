5c796b497eaf556c1bbc94a1be8f8c4cce6f42ad
Berke Kaan <bekaans@icloud.com>
2023-08-19 00:32:47 +0300

    Add Heading Class and Add Heading props

--- EntityLayer/Conctrete/Heading.cs
+++ EntityLayer/Conctrete/Heading.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Conctrete
 {
-    internal class Heading
+    public class Heading
     {
+        public int HeadingID { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
     }
 }
