e3e330e379705614fe7ef9ddb5cfd52df0bc947e
Berke Kaan <bekaans@icloud.com>
2023-08-19 01:23:10 +0300

    Add Content Key,StringLenght Attiributes

--- EntityLayer/Conctrete/Content.cs
+++ EntityLayer/Conctrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace EntityLayer.Conctrete
 {
     public class Content
     {
+        [Key]
         public int ContentID { get; set; }
+
+        [StringLength(1000)]
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
 
