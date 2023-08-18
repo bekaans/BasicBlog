41a251db06d48637b4861f70e87c2046e9bb9656
Berke Kaan <bekaans@icloud.com>
2023-08-19 01:19:59 +0300

    Add Category Key,StringLenght Attiributes

--- EntityLayer/Conctrete/Category.cs
+++ EntityLayer/Conctrete/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace EntityLayer.Conctrete
 {
     public class Category
     {
+        [Key]
         public int CategoryID { get; set; }
+        
+        [StringLength(50)]
         public string CategoryName { get; set; }
+
+        [StringLength(200)]
         public string CategoryDescription { get; set; }
         public bool CategoryStatus { get; set; }
 
