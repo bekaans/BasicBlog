7e732f7f667c7eeae335ad17c501c14e694fcb2a
Berke Kaan <bekaans@icloud.com>
2023-08-19 00:56:38 +0300

    Add About class and props

--- EntityLayer/Conctrete/About.cs
+++ EntityLayer/Conctrete/About.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Conctrete
 {
-    internal class About
+    public class About
     {
+        public int AboutID { get; set; }
+        public string AboutDetails1 { get; set; }
+        public string AboutDetails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
     }
 }
