69b7b3b5d717fc983fe0639925e330d0bd292ed7
Berke Kaan <bekaans@icloud.com>
2023-08-19 14:57:01 +0300

    Create Repositories folder and Create CategoryRepository => Repositories

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Conctrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDAL
+    {
+        public void Delete(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
