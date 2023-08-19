5c127e9d672230e68b6d437365d88f662774fccf
Berke Kaan <bekaans@icloud.com>
2023-08-19 15:13:40 +0300

    Create IRepository interface and add methods

---
+++ DataAccessLayer/Abstract/IRespository.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Conctrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRespository<T>
+    {
+        List<T> List();
+
+        //Crud Operations
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+    }
+}
