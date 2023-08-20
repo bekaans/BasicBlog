a0cf0fa4ed86fef63d9b7e61d1453398388ffe14
Berke Kaan <bekaans@icloud.com>
2023-08-21 00:57:45 +0300

    Create CategoryValidatior with Rules

---
+++ BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Conctrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidatior : AbstractValidator<Category>
+    {
+      public CategoryValidatior() 
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Do Not Pass Category Name Empty");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Do Not Pass Category Description Empty");
+            RuleFor(x => x.CategoryDescription).MinimumLength(3).WithMessage("Minimum 3 charachter");
+            RuleFor(x => x.CategoryDescription).MaximumLength(20).WithMessage("Maximum 20 charachter");
+        }
+
+    }
+}
