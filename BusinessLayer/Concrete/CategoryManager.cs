8157a0d1c370d5791f50e3293a091cdca31d444d
Berke Kaan <bekaans@icloud.com>
2023-08-20 18:01:40 +0300

    Add CategoryManager methods

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Repositories;
+using EntityLayer.Conctrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    internal class CategoryManager
+    public class CategoryManager
     {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBusinessLayer()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBusinessLayer(Category p)
+        {
+            if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            {
+                //Hata Mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
     }
 }
