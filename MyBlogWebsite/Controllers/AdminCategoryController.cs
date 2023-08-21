f45c875d9511272d976108c950ca5ea612ff19d1
Berke Kaan <bekaans@icloud.com>
2023-08-21 13:48:32 +0300

    Create AdminCategory AddAdminCategory

--- MyBlogWebsite/Controllers/AdminCategoryController.cs
+++ MyBlogWebsite/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Conctrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +14,35 @@ namespace MyBlogWebsite.Controllers
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EFCategoryDAL());
         public ActionResult Index()
         {
-            return View();
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
+                return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidatior cv= new CategoryValidatior();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+           return View();
         }
     }
 }
