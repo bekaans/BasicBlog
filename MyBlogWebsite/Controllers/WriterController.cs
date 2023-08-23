5e1e14979d77ceb0135c7c4f0a4c64238244b462
Berke Kaan <bekaans@icloud.com>
2023-08-23 23:27:55 +0300

    Update WriterController

--- MyBlogWebsite/Controllers/WriterController.cs
+++ MyBlogWebsite/Controllers/WriterController.cs
@@ -16,6 +16,7 @@ namespace MyBlogWebsite.Controllers
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EFWriterDAL());
+        WriterValidator writerValidator = new WriterValidator();
         // GET: Writer
         public ActionResult Index()
         {
@@ -30,7 +31,7 @@ namespace MyBlogWebsite.Controllers
         [HttpPost]
         public ActionResult AddWriter(Writer p) 
         {
-            WriterValidator writerValidator = new WriterValidator();
+         
             ValidationResult results = writerValidator.Validate(p);
             if (results.IsValid)
             {
@@ -56,8 +57,21 @@ namespace MyBlogWebsite.Controllers
         [HttpPost]
         public ActionResult UpdateWriter(Writer p)
         {
-            wm.WriterUpdate(p);
-            return RedirectToAction("Index");
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterUpdate(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
 
         }
     }
