3d0a7a8aaee4a4b6e66f4cdfc5535da54f859e54
Berke Kaan <bekaans@icloud.com>
2023-08-23 23:36:44 +0300

    .gitignore

---
+++ DataAccessLayer/Migrations/202308232031077_mig-addwritertittle.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migaddwritertittle : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterTittle", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Writers", "WriterTittle");
+        }
+    }
+}
