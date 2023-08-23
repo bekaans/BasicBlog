33a960c178f4bf9fd2b185ca2b3c245ad4d3b20b
Berke Kaan <bekaans@icloud.com>
2023-08-23 23:54:02 +0300

    Create HeadingController and Index View

---
+++ MyBlogWebsite/Controllers/HeadingController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogWebsite.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EFHeadingDAL());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+    }
+}
