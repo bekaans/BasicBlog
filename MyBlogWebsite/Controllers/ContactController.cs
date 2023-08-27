6af1ba1c0202c3eeb908da073ac7f6ba48969a42
Berke Kaan <bekaans@icloud.com>
2023-08-28 00:42:52 +0300

    Add ContactController Method and Index and PartialView

--- MyBlogWebsite/Controllers/ContactController.cs
+++ MyBlogWebsite/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,22 @@ namespace MyBlogWebsite.Controllers
 {
     public class ContactController : Controller
     {
+        ContactManager cm = new ContactManager(new EFContactDAL());
+        ContactValidator cv = new ContactValidator();
         // GET: Contact
         public ActionResult Index()
         {
-            return View();
+            var contactvalue = cm.GetList();
+            return View(contactvalue);
+        }
+        public ActionResult getContactDetails(int id)
+        {
+            var contactvalue = cm.GetById(id);
+            return View(contactvalue);
+        }
+        public PartialViewResult ContactPartial()
+        {
+            return PartialView();
         }
     }
 }
