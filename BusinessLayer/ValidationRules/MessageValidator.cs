8a6341b88aa9032c9fc42d0376495cc96ac00c90
Berke Kaan <bekaans@icloud.com>
2023-08-28 16:58:42 +0300

    Create MessageValidator

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Conctrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator:AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+        RuleFor(x => x.MessageReceiver).NotEmpty().WithMessage("Do Not Pass Category Name Empty");
+        RuleFor(x => x.MessageSender).NotEmpty().WithMessage("Do Not Pass Category Name Empty");
+        RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Do Not Pass Category Description Empty");
+        RuleFor(x => x.MessageSender).MinimumLength(3).WithMessage("Minimum 3 charachter");
+        RuleFor(x => x.MessageReceiver).MinimumLength(3).WithMessage("Minimum 3 charachter");
+        RuleFor(x => x.MessageSender).MaximumLength(20).WithMessage("Maximum 20 charachter");
+        RuleFor(x => x.MessageReceiver).MaximumLength(20).WithMessage("Maximum 20 charachter");
+        }
+        
+       
+    }
+}
