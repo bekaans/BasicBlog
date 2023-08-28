4c48f32f11ad423637fe67d64ae67269c217efbc
Berke Kaan <bekaans@icloud.com>
2023-08-28 15:29:45 +0300

    .gitignore

---
+++ DataAccessLayer/Migrations/202308281212035_mig_add_messageclass.cs
@@ -0,0 +1,30 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_add_messageclass : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        MessageID = c.Int(nullable: false, identity: true),
+                        MessageSender = c.String(maxLength: 50),
+                        MessageReceiver = c.String(maxLength: 50),
+                        MessageSubject = c.String(maxLength: 250),
+                        MessageContent = c.String(),
+                        MessageDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.MessageID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Messages");
+        }
+    }
+}
