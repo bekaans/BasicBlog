8f15577e5aab3725c00af71aa7ddd44b45bcd642
Berke Kaan <bekaans@icloud.com>
2023-08-28 15:28:07 +0300

    New EntityLayer class=> Message and add-migration

---
+++ EntityLayer/Conctrete/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Conctrete
+{
+    public class Message
+    {
+        [Key]
+        public int MessageID { get; set; }
+        [StringLength(50)]
+        public string MessageSender { get; set; }
+        [StringLength(50)]
+        public string MessageReceiver { get; set; }
+        [StringLength(250)]
+        public string MessageSubject { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
