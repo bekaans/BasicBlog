bd14a1d33935fc6737dcf801033f764eaa119fcb
Berke Kaan <bekaans@icloud.com>
2023-08-28 16:59:15 +0300

    Update MessageController

--- MyBlogWebsite/Controllers/MessageController.cs
+++ MyBlogWebsite/Controllers/MessageController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Conctrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +11,26 @@ namespace MyBlogWebsite.Controllers
 {
     public class MessageController : Controller
     {
+        MessageManager mm = new MessageManager(new EFMessageDAL());
+
         // GET: Message
         public ActionResult Inbox()
+        {
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+        public ActionResult Sendbox() 
+        {
+            var messagelist = mm.GetListSendBox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage() 
+        { 
+                return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
         {
             return View();
         }
