489e725a561df703b7711cbc754a1b8dfd976c40
Berke Kaan <bekaans@icloud.com>
2023-08-28 15:40:42 +0300

    Create IMessageService.cs and methods

---
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Conctrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetList();
+        void MessageAdd(Message message);
+        Message GetById(int id);
+        void MessageRemove(Message message);
+        void MessageUpdate(Message message);
+    }
+}
