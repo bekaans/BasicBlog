5a0bc5b053fce1afe488a61a43544aece9e349a1
Berke Kaan <bekaans@icloud.com>
2023-08-31 21:25:02 +0300

    Update GalleryController method

--- MyBlogWebsite/Controllers/GalleryController.cs
+++ MyBlogWebsite/Controllers/GalleryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,15 @@ using System.Web.Mvc;
 
 namespace MyBlogWebsite.Controllers
 {
+  
     public class GalleryController : Controller
     {
+        ImageFileManager im = new ImageFileManager(new EFImageFileDAL());
         // GET: Gallery
         public ActionResult Index()
         {
-            return View();
+            var files = im.GetList();
+            return View(files);
         }
     }
 }
