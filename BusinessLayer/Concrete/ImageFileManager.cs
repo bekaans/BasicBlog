a6faf0f5c5956dd9ed044f561c4fc463ed63c07e
Berke Kaan <bekaans@icloud.com>
2023-08-31 21:38:04 +0300

    Create ImageFileManager

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Conctrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDAL _imagefiledal;
+
+        public ImageFileManager(IImageFileDAL imagefiledal)
+        {
+            _imagefiledal = imagefiledal;
+        }
+
+        public List<ImageFile> GetList()
+        {
+            return _imagefiledal.List();
+        }
+    }
+}
