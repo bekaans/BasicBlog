6e463f31cfe23fde473a8ddd56e46c3790f6a8cd
Berke Kaan <bekaans@icloud.com>
2023-08-31 21:24:45 +0300

    Create EFImaFileDAL => DataAccessLayer/EntityFramework

---
+++ DataAccessLayer/EntityFramework/EFImageFileDAL.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Conctrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EFImageFileDAL : GenericRepository<ImageFile>, IImageFileDAL
+    {
+
+    }
+}
