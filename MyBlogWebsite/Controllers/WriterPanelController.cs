403cc26faf72bd14bc5dd1baaf2a2982b6c710d0
Berke Kaan <bekaans@icloud.com>
2023-09-01 16:21:14 +0300

    Add MyHeading method

--- MyBlogWebsite/Controllers/WriterPanelController.cs
+++ MyBlogWebsite/Controllers/WriterPanelController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,24 @@ namespace MyBlogWebsite.Controllers
 {
     public class WriterPanelController : Controller
     {
+        HeadingManager hm = new HeadingManager(new EFHeadingDAL());
         // GET: WriterPanel
         public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        public ActionResult MyHeading()
+        {
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading()
         {
             return View();
         }
