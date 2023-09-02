6119a25070d80b12d2941136929fca710f07a288
Berke Kaan <bekaans@icloud.com>
2023-09-02 22:41:59 +0300

    Add NewMessage method and views => WriterPanelMessage

--- MyBlogWebsite/Controllers/WriterPanelMessageController.cs
+++ MyBlogWebsite/Controllers/WriterPanelMessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Conctrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +39,31 @@ namespace MyBlogWebsite.Controllers
         {
             var value = mm.GetById(id);
             return View(value);
+        }
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+
+            ValidationResult results = messageValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("Sendbox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
         }
     }
 }
