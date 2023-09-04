b3e925b6555c4cd267871fb393907bd0593100fc
Berke Kaan <bekaans@icloud.com>
2023-09-04 10:20:58 +0300

    Create DefaultController

---
+++ MyBlogWebsite/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogWebsite.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Headings()
+        {
+            return View();
+        }
+    }
+}
